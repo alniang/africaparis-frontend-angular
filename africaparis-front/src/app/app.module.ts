1abc0535960186cf6726852971bf0bb095fc4ea2
Alpha Niang <alpha.niang@hotmail.com>
2020-06-19 00:34:36 +0200

    création de footer, header et evenement

--- africaparis-front/src/app/app.module.ts
+++ africaparis-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { EvenementComponent } from './evenement/evenement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    EvenementComponent
   ],
   imports: [
     BrowserModule,
